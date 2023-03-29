e3b570840d854f76cc5536d1e030d166e995fe49
janani <jayaramanjanani73@gmail.com>
2023-03-29 14:08:38 +0530

    youtube updated

--- index.js
+++ index.js
@@ -1,87 +1,105 @@
-//'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=[YOUR_API_KEY]'
+// const API_key = `AIzaSyCKspbCf-Bya0SElHZXgQvHUWOUAFSqG6k`;
 
 
+const API_key= `AIzaSyC63RHmWuZg9mYGoEQ2v-FHfJvPIUiyl_w`
 
+const container_div = document.getElementById("container")
 
+const SearchVideos = async () => {
 
+try{
 
-const container_div=document.getElementById("video_details")
-
-const api=`AIzaSyC63RHmWuZg9mYGoEQ2v-FHfJvPIUiyl_w`
-
-const searchVideos=async()=>{
-
-    try{
-        const query =document.getElementById('query').value
-
-     const res=await fetch (`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${query}&key=${api}`)
+    const query = document.getElementById("query").value;
     
-    const data =await res.json()
 
-    const actual_data=data.items;
+   const res = await fetch (`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${query}&key=${API_key}`)
+   const data = await res.json();
+   console.log(data);
+   
+  
+   const actual_data = data.items;
+   AppendVideos(actual_data)
 
-    appendVideos(actual_data)
-     console.log(data)
 
-    }
-    catch(error){
-        console.log(error)
-    }
+}catch(error){
+
+console.log(error)
+}
 
 
 }
+// data is array of obj
+const AppendVideos = (data) =>{
 
-//searchVideos()
-
-const appendVideos = (data)=>{
-
-    container_div.innerHTML=null
-    data.forEach(({snippet ,id})=>{
-
-        const title=snippet.title;
-
-        const videoId= id.videoId;
-    
-        const thumbnail=snippet.thumbnails.high.url;
-    
-        const channel_name= snippet.channelTitle;
-        
-    
-        const div=document.createElement("div");
-    
-        const img=document.createElement("img");
-        img.src=thumbnail
-    
-        const title_html=document.createElement("h4");
-         title_html.innerText=title
-    
-        const channel_html=document.createElement("h5");
-         channel_html.innerText=channel_name;
-
-         let data = {
-
-            videoId,
-            snippet,
-        
-         };
-        
-         div.onclick = ()=>{
-              storeclickVideo(data);
-        }
-        //  div.onclick=()=>{
-        //     //console.log("clicked")
-        //  }
-
-         div.append(img,title_html,channel_html);
-        
-         container_div.append(div)
-    })
  
+    container_div.innerHTML = null;
+
+data.forEach( ({snippet ,id})  => {
+    // console.log(el);
+
   
 
+    const title = snippet.title;
+
+    const videoId  = id.videoId;
+
+    const thumbnail = snippet.thumbnails.high.url;
+
+
+    const channel_name = snippet.channelTitle;
+
+    // console.log(el)
+
+    const div = document.createElement("div");
+    const img = document.createElement("img");
+
+    img.src = thumbnail;
+   
+    const title_html = document.createElement("h4");
+
+    title_html.innerText = title;
+    title_html.style.color  ="white";
+    title_html.style.fontFamily =  'math';
+ 
+    title_html.style.fontWeight = 200;
+
+    const channel_html = document.createElement("h5");
+    channel_html.innerText = channel_name;
+    channel_html.style.color  ="white";
+ 
+
+//for second page
+    let data = {
+
+       videoId,
+       snippet,
+
+    };
+
+    div.onclick = ()=>{
+    
+        storeclickVideo(data);
+
+    }
+
+
+    div.append(img,title_html,channel_html);
+
+   container_div.append(div)
+
+
+})
+
+
 }
 
 
+function storeclickVideo(data){
+
+localStorage.setItem("clicked_item",JSON.stringify(data));
+window.location.href = "video.html";
+
+}
 
 
 const showVideo = (x)=>{
@@ -93,11 +111,77 @@ x = JSON.stringify(x);
 localStorage.setItem("clicked_video",x)
 }
 
-function storeclickVideo(data){
 
-    //console.log(data)
 
-    localStorage.setItem("clicked_item",JSON.stringify(data));
-    window.location.href = "video.html";
-    
-    }
+
+
+
+    async function ab(){
+
+        try{
+
+            
+        
+            let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyC63RHmWuZg9mYGoEQ2v-FHfJvPIUiyl_w&maxResults=20`);
+
+     
+        let data = await res.json();
+       
+        
+        let items = data.items;
+        
+        console.log("item:",items )
+        
+        appendVideo(items)
+  
+        
+        }catch(error){
+        
+            console.log(error)
+        }
+        
+        
+        
+        }
+        
+        ab()
+        
+        const appendVideo = (items) =>{
+        
+        
+        
+        items.map((el)=>{
+        
+        let {snippet,id:{videoId}}=el
+        
+        console.log(snippet);
+
+        let tit = snippet.title;
+
+        
+        
+       let title = document.createElement("h4");
+       title.innerText = tit;
+        
+
+
+        let iframe =  document.createElement("iframe");
+        iframe.src = `https://www.youtube.com/embed/${videoId}`
+        iframe.height = "180px";
+        iframe.width = "90%";
+        iframe.style.border = "1px solid black";
+        iframe.style.marginTop = "10px";
+       
+        
+        
+        let div = document.createElement("div");
+        
+        div.append(iframe,title);
+        
+        document.getElementById("container").append(div)
+        
+        })
+        
+        
+        } 
+        
--- video.js
+++ video.js
@@ -1,35 +1,101 @@
 
-// const playVideo=() =>{
+//  const API_key = `AIzaSyC63RHmWuZg9mYGoEQ2v-FHfJvPIUiyl_w`;
 
-//     let data=JSON.parse(localStorage.getItem("clicked_video"));
 
-//     console.log(data);
-// }
-
-const api = `AIzaSyC63RHmWuZg9mYGoEQ2v-FHfJvPIUiyl_w`;
 
 const video_details = document.getElementById("video_details") 
-console.log(video_details)
 
 const playVideo = () =>{
 
-let {videoId} = JSON.parse(localStorage.getItem("clicked_item"))
+let {snippet,videoId} = JSON.parse(localStorage.getItem("clicked_item"))
 
-//console.log(data);
+// console.log(data);
 
+let title = snippet.title;
+let description = snippet.description;
 
 let iframe = document.createElement("iframe");
 
+let tit = document.createElement("h2");
+tit.innerText = title;
+ let des = document.createElement("p");
+ des.innerText = description;
+
+
+
 iframe.src= `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`;
-iframe.width = "100%";
-iframe.height = "100%";
-// iframe.style.marginTop = "50px";
-// iframe.style.border = "3px solid black";
+iframe.width = "90%";
+iframe.height = "70%";
+iframe.style.marginTop = "50px";
+iframe.style.border = "3px solid black";
 iframe.setAttribute("allowfullscreen",true);
 
-video_details.append(iframe);
+video_details.append(iframe,tit,des);
 
 
 
 
 };
+
+
+async function ab(){
+
+try{
+
+let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyC63RHmWuZg9mYGoEQ2v-FHfJvPIUiyl_w&maxResults=3
+`);
+
+let data = await res.json();
+
+let items = data.items;
+
+console.log("item:",items )
+
+appendVideo(items)
+
+}catch(error){
+
+    console.log(error)
+}
+
+
+
+}
+
+ab()
+
+const appendVideo = (items) =>{
+
+
+
+items.map((el)=>{
+
+let {snippet,id:{videoId}}=el
+
+console.log(snippet);
+
+let iframe =  document.createElement("iframe");
+iframe.src = `https://www.youtube.com/embed/${videoId}?`;
+
+iframe.height = "177px";
+iframe.width = "100%";
+iframe.margin = "auto";
+iframe.style.border = "1px solid black";
+
+
+let div = document.createElement("div");
+
+div.append(iframe);
+
+document.getElementById("recommend").append(div)
+
+})
+
+
+} 
+
+
+
+
+
+
